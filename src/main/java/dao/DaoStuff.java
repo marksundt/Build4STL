6b73a17af343b09f9f23b39b6b0d26c46bf650bd
James Wall <james.wall@wustl.edu>
2015-05-30 15:14:37 -0500

    James- method takes List of tokens as input

--- src/main/java/dao/DaoStuff.java
+++ src/main/java/dao/DaoStuff.java
@@ -8,57 +8,62 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 
 public class DaoStuff {
-	
-	public static String executePost(String targetURL, String token) {
-		  HttpURLConnection connection = null;  
-		  try {
-		    //Create connection
-		    URL url = new URL(targetURL);
-		    connection = (HttpURLConnection)url.openConnection();
-		    connection.setRequestMethod("PUT");
-		    connection.setRequestProperty("Content-Type", 
-		        "application/json");
-		   // String body = "{\"name\":" + "\"" + name + "\"," + "\"email\":" + "\"" + email + "\"," + "\"phoneNumber\":" + "\"" + phoneNum + "\"}";
-		    String body = "{\"userID_token\":" + "\"" + token + "\"}";
-		    //System.out.println(body);
-		    String basicAuth = "Basic YmE2YzEwM2YtOTY2ZS00Yzc2LWIwY2UtNjI0ODQ0YTI3OTU1Og==";
-		    connection.setRequestProperty ("Authorization", basicAuth);
-		    connection.setRequestProperty("Content-Length", 
-		        Integer.toString(body.getBytes().length));
-		    connection.setRequestProperty("Content-Language", "en-US");  
 
-		    connection.setUseCaches(false);
-		    connection.setDoOutput(true);
-		    
-		    //Send request
-		    DataOutputStream wr = new DataOutputStream (
-		        connection.getOutputStream());
-		    wr.writeBytes(body);
-		    wr.close();
+	public static void executePost(String targetURL, List<String> tokens) {
+		HttpURLConnection connection = null;  
+		try {
+			for(int i = 0; i < tokens.size(); i++){
+				//Create connection
+				URL url = new URL(targetURL);
+				connection = (HttpURLConnection)url.openConnection();
+				connection.setRequestMethod("PUT");
+				connection.setRequestProperty("Content-Type", 
+						"application/json");
+				// String body = "{\"name\":" + "\"" + name + "\"," + "\"email\":" + "\"" + email + "\"," + "\"phoneNumber\":" + "\"" + phoneNum + "\"}";
+				String body = "{\"userID_token\":" + "\"" + tokens.get(i) + "\"}";
+				//System.out.println(body);
+				String basicAuth = "Basic YmE2YzEwM2YtOTY2ZS00Yzc2LWIwY2UtNjI0ODQ0YTI3OTU1Og==";
+				connection.setRequestProperty ("Authorization", basicAuth);
+				connection.setRequestProperty("Content-Length", 
+						Integer.toString(body.getBytes().length));
+				connection.setRequestProperty("Content-Language", "en-US");  
 
-		    //Get Response  
-		    InputStream is = connection.getInputStream();
-		    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-		    StringBuilder response = new StringBuilder(); // or StringBuffer if not Java 5+ 
-		    String line;
-		    while((line = rd.readLine()) != null) {
-		      response.append(line);
-		      response.append('\r');
-		    }
-		    rd.close();
-		    return response.toString();
-		  } catch (Exception e) {
-		    e.printStackTrace();
-		    return null;
-		  } finally {
-		    if(connection != null) {
-		      connection.disconnect(); 
-		    }
-		  }
+				connection.setUseCaches(false);
+				connection.setDoOutput(true);
+
+				//Send request
+				DataOutputStream wr = new DataOutputStream (
+						connection.getOutputStream());
+				wr.writeBytes(body);
+				wr.close();
+
+				//Get Response  
+				InputStream is = connection.getInputStream();
+				BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+				StringBuilder response = new StringBuilder(); // or StringBuffer if not Java 5+ 
+				String line;
+				while((line = rd.readLine()) != null) {
+					response.append(line);
+					response.append('\r');
+				}
+				rd.close();
+//				return response.toString();
+			}
+
 		}
+		catch (Exception e) {
+			e.printStackTrace();
+			//return null;
+		} finally {
+			if(connection != null) {
+				connection.disconnect(); 
+			}
+		}
+	}
 
 
 }
