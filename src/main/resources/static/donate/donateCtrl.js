a6fca13cbad6445bfc55dc6c10e089919f810610
carmbruster <carmbruster@clearent.com>
2015-05-30 16:19:02 -0500

    got it working

--- src/main/resources/static/donate/donateCtrl.js
+++ src/main/resources/static/donate/donateCtrl.js
@@ -1,11 +1,18 @@
 'use strict';
 angular
     .module('WebApp')
-    .controller('DonateCtrl', ['$scope', '$http', 'AuthService', function($scope, $http, AuthService){
+    .controller('DonateCtrl', ['$scope', '$http', 'AuthService','$location', function($scope, $http, AuthService,$location){
 
         var vm = this;
         vm.isAuth = isAuth;
 
+        var auth = AuthService.getAuth();
+        if(!auth.authenticated){
+            $location.path('/login');
+        }
+
+        $scope.name= auth.name;
+        $scope.newCard=false;
 
         // Checks to see if the user is authenticated
         function isAuth(){
