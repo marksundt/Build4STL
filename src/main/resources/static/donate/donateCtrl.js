6765f1b2501c3635deebbba9248825f9990fe56d
carmbruster <carmbruster@clearent.com>
2015-05-30 16:47:31 -0500

    got it working

--- src/main/resources/static/donate/donateCtrl.js
+++ src/main/resources/static/donate/donateCtrl.js
@@ -13,6 +13,19 @@ angular
 
         $scope.name= auth.name;
         $scope.newCard=false;
+        $scope.dropDownLabel = 'Select donation payment method';
+        $scope.donationSuccessful = false;
+
+        $scope.setCard = function(card){
+            if (card=="new"){
+                $scope.dropDownLabel = 'Donate using another card';
+                $scope.newCard=true;
+            }else{
+                $scope.dropDownLabel = 'Donate using card ending in ' + card;
+                $scope.newCard=false;
+            }
+
+        };
 
         // Checks to see if the user is authenticated
         function isAuth(){
